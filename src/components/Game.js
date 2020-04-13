b825f4a34b5ad6fc7d4cffc4df9afda13d5e2abf
Jim Ntare <jim.ntare@gmail.com>
2020-04-13 20:50:07 +0200

    create tick tack toe game with hooks

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,21 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { calculateWinner } from '../helpers';
 import Board from './Board'
 
+const styles = {
+  width: '200px',
+  margin: '20px auto',
+};
+
 const Game = () => {
-  const handleClick = () => {
+  const [history, setHistory] = useState(Array(9).fill(null));
+  const [xIsNext, setXisNext] = useState(true);
 
+  const winner = calculateWinner(history)
+
+  const handleClick = (i) => {
+    const boardCopy = [...history];
+    // if user clicks on occupied square or if game is won, return
+    if (winner || boardCopy[i]) return;
+    // Put and X or an O in the clicked square
+    boardCopy[i] = xIsNext ? 'X' : 'O';
+    setHistory(boardCopy);
+    
+    setXisNext(!xIsNext)
   }
 
-  const jumpTo = () => {
-
-  }
 
   const renderMoves = () => {
-    
+    return <button onClick={() => setHistory(Array(9).fill(null))}>Start Game</button>
   }
 
   return (
-    <Board onClick={handleClick} />
+    <>
+      <Board squares={history} onClick={handleClick} />
+      <div style={styles}>
+        <p>{winner ? 'Winner ' + winner : 'Next Player' + (xIsNext ? ' X' : ' O')}</p>
+        {renderMoves()}
+      </div>
+
+    </>
   )
 }
 
